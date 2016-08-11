def063dfe3ca172871840c042bb78116caad6f17
nonylene <cookie.speaker@gmail.com>
2016-08-11 23:37:47 +0900

    user valid test 1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,13 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  #
+  def setup
+    @user = User.new(name: "nonylene", email: "nonylene@example.com")
+  end
+
+  test "should be vaild" do 
+    assert @user.valid?
+  end
+
 end
